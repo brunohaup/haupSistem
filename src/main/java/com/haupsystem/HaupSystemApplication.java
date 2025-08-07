3212a72294541369fde6de07e9e55f26452d039c
Bruno Haupenthal <brunohaupenthal99@gmail.com>
2025-08-07 18:12:51 -0300

    primeiro commit

---
+++ src/main/java/com/haupsystem/HaupSystemApplication.java
@@ -0,0 +1,13 @@
+package com.haupsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaupSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaupSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/haupsystem/HaupSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haupsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaupSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
